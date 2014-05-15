c790706ecf973121cb51e2546f7e968d58c89fb2
sniper <linfeng.fjut@gmail.com>
2014-05-15 17:48:10 +0800

    first commit

---
+++ BaiduPanSearch/SearchForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace BaidupanSearch
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.lvResult = new System.Windows.Forms.ListView();
+            this.标题 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.cmdPrevious = new System.Windows.Forms.Button();
+            this.cmdNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdSearch
+            // 
+            this.cmdSearch.Location = new System.Drawing.Point(440, 24);
+            this.cmdSearch.Name = "cmdSearch";
+            this.cmdSearch.Size = new System.Drawing.Size(75, 25);
+            this.cmdSearch.TabIndex = 0;
+            this.cmdSearch.Text = "搜索";
+            this.cmdSearch.UseVisualStyleBackColor = true;
+            this.cmdSearch.Click += new System.EventHandler(this.cmdSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "搜索关键字 ：";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(103, 26);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(321, 21);
+            this.tbKey.TabIndex = 2;
+            // 
+            // lvResult
+            // 
+            this.lvResult.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.lvResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.标题});
+            this.lvResult.GridLines = true;
+            this.lvResult.Location = new System.Drawing.Point(12, 98);
+            this.lvResult.MultiSelect = false;
+            this.lvResult.Name = "lvResult";
+            this.lvResult.ShowItemToolTips = true;
+            this.lvResult.Size = new System.Drawing.Size(740, 387);
+            this.lvResult.TabIndex = 3;
+            this.lvResult.UseCompatibleStateImageBehavior = false;
+            this.lvResult.View = System.Windows.Forms.View.Details;
+            this.lvResult.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvResult_MouseDoubleClick);
+            // 
+            // 标题
+            // 
+            this.标题.Text = "标题";
+            this.标题.Width = 660;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "搜索结果 ：";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(91, 65);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 12);
+            this.lbResult.TabIndex = 5;
+            // 
+            // cmdPrevious
+            // 
+            this.cmdPrevious.Enabled = false;
+            this.cmdPrevious.Location = new System.Drawing.Point(544, 24);
+            this.cmdPrevious.Name = "cmdPrevious";
+            this.cmdPrevious.Size = new System.Drawing.Size(75, 25);
+            this.cmdPrevious.TabIndex = 6;
+            this.cmdPrevious.Text = "上一页";
+            this.cmdPrevious.UseVisualStyleBackColor = true;
+            this.cmdPrevious.Click += new System.EventHandler(this.cmdPrevious_Click);
+            // 
+            // cmdNext
+            // 
+            this.cmdNext.Enabled = false;
+            this.cmdNext.Location = new System.Drawing.Point(650, 24);
+            this.cmdNext.Name = "cmdNext";
+            this.cmdNext.Size = new System.Drawing.Size(75, 25);
+            this.cmdNext.TabIndex = 7;
+            this.cmdNext.Text = "下一页";
+            this.cmdNext.UseVisualStyleBackColor = true;
+            this.cmdNext.Click += new System.EventHandler(this.cmdNext_Click);
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 497);
+            this.Controls.Add(this.cmdNext);
+            this.Controls.Add(this.cmdPrevious);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lvResult);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdSearch);
+            this.Name = "SearchForm";
+            this.Text = "百度网盘搜索";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.ListView lvResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.ColumnHeader 标题;
+        private System.Windows.Forms.Button cmdPrevious;
+        private System.Windows.Forms.Button cmdNext;
+    }
+}
+
---
+++ BaiduPanSearch/SearchForm.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace BaidupanSearch
+{
+    public partial class SearchForm : Form
+    {
+        private static string browser = "";
+        private static readonly int rn = 35;
+        private static readonly int pagesize = 30;
+        private static int total = 0;
+        private static int page = 1;
+        private static int totalpage = 0;
+        private static string key = "";
+
+        static SearchForm()
+        {
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(@"http\shell\open\command\");
+            browser = key.GetValue("").ToString().Split(new string[] { " -" }, StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+
+        static void OpenLink(string url)
+        {
+            System.Diagnostics.Process.Start(browser, url);
+        }
+
+        static string HttpGet(string url)
+        {
+            string result = "";
+            MemoryStream ms = null;
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "GET";
+
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+                if (req.HaveResponse)
+                {
+                    ms = new MemoryStream();
+                    res.GetResponseStream().CopyTo(ms);
+                    result = Encoding.GetEncoding(res.ContentEncoding == "" ? "utf-8" : res.ContentEncoding).GetString(ms.ToArray());
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (ms != null)
+                {
+                    ms.Close();
+                }
+            }
+
+            return result;
+        }
+
+        static void GetTotal(string total, out int count)
+        {
+            string result = "";
+
+            MatchCollection ms = Regex.Matches(total, "\\d+");
+            if (ms != null)
+            {
+                foreach (var m in ms)
+                {
+                    result += m.ToString();
+                }
+            }
+
+            int.TryParse(result, out count);
+        }
+
+        public SearchForm()
+        {
+            InitializeComponent();
+
+            //Set ListView Item Row Height
+            ImageList imgList = new ImageList();
+            imgList.ImageSize = new Size(1, 21);
+            lvResult.SmallImageList = imgList;
+        }
+
+        private void SetTotal(int total, int page, int totalpage)
+        {
+            lbResult.Text = string.Format("共 {0} 条 , 当前第 {1} 页 , 共 {2} 页 ", total, page, totalpage);
+        }
+
+        private void Reset()
+        {
+            lbResult.Text = "";
+            key = "";
+            total = 0;
+            page = 1;
+            totalpage = 0;
+        }
+
+        private void Search()
+        {
+            lvResult.BeginUpdate();
+
+            try
+            {
+                string url = string.Format("http://www.baidu.com/s?wd=site:pan.baidu.com%20{0}&pn={1}&rn={2}", key, (page - 1) * rn, rn);
+                string result = HttpGet(url);
+
+                if (!string.IsNullOrEmpty(result))
+                {
+                    HtmlAgilityPack.HtmlDocument hd = new HtmlAgilityPack.HtmlDocument();
+                    hd.LoadHtml(result);
+
+                    var aTag = hd.DocumentNode.SelectNodes("//a[@data-click]");
+                    var divTag = hd.DocumentNode.SelectNodes("//div[@class='c-abstract']");
+
+                    if (aTag != null && divTag != null)
+                    {
+                        var aTags = aTag.ToArray();
+                        var divTags = divTag.ToArray();
+
+                        for (int i = 0; i < aTags.Length; i++)
+                        {
+                            var a = aTags[i];
+                            var div = divTags[i];
+
+                            ListViewItem lvi = new ListViewItem();
+
+                            lvi.Text = "  " + (i + 1).ToString() + "、  " + a.InnerText.Replace("|百度云 网盘-分享无限制", "");
+                            lvi.ToolTipText = div.InnerText;
+                            lvi.SubItems.Add(a.Attributes["href"].Value);
+
+                            lvResult.Items.Add(lvi);
+                        }
+
+                        var nums = hd.DocumentNode.SelectSingleNode("//span[@class='nums']");
+                        GetTotal(nums.InnerText, out total);
+
+                        totalpage = total % pagesize == 0 ? total / pagesize : total / pagesize + 1;
+                        cmdNext.Enabled = totalpage > page;
+                        SetTotal(total, page, totalpage);
+                    }
+                    else
+                    {
+                        SetTotal(0, 0, 0);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            lvResult.EndUpdate();
+        }
+
+        private void cmdSearch_Click(object sender, EventArgs e)
+        {
+            if (tbKey.Text == "") return;
+
+            Reset();
+            lvResult.Items.Clear();
+            key = tbKey.Text;
+            Search();
+        }
+
+        private void lvResult_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var lv = sender as ListView;
+            if (lv.SelectedItems.Count > 0)
+            {
+                var lvi = lv.SelectedItems[0];
+                OpenLink(lvi.SubItems[1].Text);
+            }
+        }
+
+        private void cmdPrevious_Click(object sender, EventArgs e)
+        {
+            page--;
+            lvResult.Items.Clear();
+            Search();
+
+            cmdPrevious.Enabled = page != 1;
+            cmdNext.Enabled = true;
+        }
+
+        private void cmdNext_Click(object sender, EventArgs e)
+        {
+            page++;
+            lvResult.Items.Clear();
+            Search();
+
+            cmdNext.Enabled = page != pagesize;
+            cmdPrevious.Enabled = true;
+        }
+
+    }
+}
