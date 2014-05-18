59f55e94a7a5ae7be9571e53f44f2f23ddbff705
firesharpv <linfeng.fjut@gmail.com>
2014-05-18 23:11:23 +0800

    修改主窗体ICO

--- BaiduPanSearch/SearchForm.Designer.cs
+++ BaiduPanSearch/SearchForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchForm));
             this.cmdSearch = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tbKey = new System.Windows.Forms.TextBox();
@@ -169,6 +170,7 @@
             this.Controls.Add(this.tbKey);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmdSearch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "SearchForm";
             this.Text = "百度网盘搜索";
