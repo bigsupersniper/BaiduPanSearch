ef00b0e4473d6abe6168e59e442a63768238e929
bigsupersniper <linfeng.fjut@gmail.com>
2015-04-16 17:30:09 +0800

    删除旧版基于百度搜索引擎的实现 … 新建基于NET45的WPF项目实现 添加必应与谷歌搜索引擎实现

---
+++ BaiduPanSearch.NET45/Engine/Bing.cs
@@ -0,0 +1,176 @@
+﻿using BaiduPanSearch.NET45.Models;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BaiduPanSearch.NET45.Engine
+{
+    /// <summary>
+    /// 必应搜索引擎
+    /// </summary>
+    public class Bing : SearchBase
+    {
+        /// <summary>
+        /// 搜索URL模板
+        /// </summary>
+        const string SearchUrlTemplate = "http://cn.bing.com/search?q=site%3apan.baidu.com+{0}&first={1}";
+
+        public Bing()
+        {
+            base.PageSize = 10;
+        }
+
+        List<GridRowItem> ParseResult(HtmlNode hn)
+        {
+            if (hn != null)
+            {
+                var h2s = hn.SelectNodes("li/h2/a");
+
+                if (h2s != null && h2s.Count > 0)
+                {
+                    var ls = new List<GridRowItem>();
+                    foreach (var h2 in h2s)
+                    {
+                        string title = HttpUtility.HtmlDecode(h2.InnerText);
+                        string url = base.ParseUrl(HttpUtility.HtmlDecode(h2.Attributes["href"].Value));
+
+                        ls.Add(new GridRowItem
+                        {
+                            Title = title,
+                            Url = url
+                        });
+                    }
+
+                    if (!base.Cached.ContainsKey(base.CurrentPage))
+                    {
+                        base.Cached.Add(base.CurrentPage, ls);
+                    }
+
+                    return ls;
+                }
+            }
+
+            return null;
+        }
+
+        public override async Task<List<GridRowItem>> Search(string keyword)
+        {
+            List<GridRowItem> ls = null;
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                string url = string.Format(SearchUrlTemplate, keyword, 1);
+                using (var client = new HttpClient())
+                {
+                    var res = await client.GetAsync(url);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string html = await res.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            var hd = new HtmlDocument();
+                            hd.LoadHtml(html);
+                            var b_results = hd.GetElementbyId("b_results");
+                            if (b_results != null)
+                            {
+                                var sb_count = b_results.SelectSingleNode("li/span[@class='sb_count']");
+                                if (sb_count != null)
+                                {
+                                    base.ResultStatus = "共" + sb_count.InnerText;
+                                    base.TotalPage = ParseTotal(sb_count.InnerText) / PageSize;
+                                }
+
+                                ls = ParseResult(b_results);
+                            }
+                        }
+
+                        base.Keyword = keyword;
+                    }
+                }
+            }
+
+            return ls;
+        }
+
+        public override async Task<List<GridRowItem>> PageUp()
+        {
+            List<GridRowItem> ls = null;
+
+            if (base.Cached.ContainsKey(CurrentPage - 1))
+            {
+                CurrentPage--;
+                ls = base.Cached[CurrentPage];
+            }
+            else if (CurrentPage <= TotalPage)
+            {
+                int first = (CurrentPage - 2) * PageSize + 1;
+                string url = string.Format(SearchUrlTemplate, base.Keyword, first);
+
+                using (var client = new HttpClient())
+                {
+                    var res = await client.GetAsync(url);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string html = await res.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            var hd = new HtmlDocument();
+                            hd.LoadHtml(html);
+                            var b_results = hd.GetElementbyId("b_results");
+                            if (b_results != null)
+                            {
+                                CurrentPage--;
+                                ls = ParseResult(b_results);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ls;
+        }
+
+        public override async Task<List<GridRowItem>> PageDown()
+        {
+            List<GridRowItem> ls = null;
+
+            if (base.Cached.ContainsKey(CurrentPage + 1))
+            {
+                CurrentPage++;
+                ls = base.Cached[CurrentPage];
+            }
+            else if (CurrentPage >= 1)
+            {
+                int first = CurrentPage * PageSize + 1;
+                string url = string.Format(SearchUrlTemplate, base.Keyword, first);
+
+                using (var client = new HttpClient())
+                {
+                    var res = await client.GetAsync(url);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string html = await res.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            var hd = new HtmlDocument();
+                            hd.LoadHtml(html);
+                            var b_results = hd.GetElementbyId("b_results");
+                            if (b_results != null)
+                            {
+                                CurrentPage++;
+                                ls = ParseResult(b_results);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ls;
+        }
+    }
+}
---
+++ BaiduPanSearch.NET45/Engine/Google.cs
@@ -0,0 +1,190 @@
+﻿using BaiduPanSearch.NET45.Models;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BaiduPanSearch.NET45.Engine
+{
+    /// <summary>
+    /// 谷歌搜索引擎
+    /// </summary>
+    public class Google : SearchBase
+    {
+        /// <summary>
+        /// 搜索URL模板
+        /// </summary>
+        const string SearchUrlTemplate = @"https://www.google.com.hk/search?q=site:pan.baidu.com+{0}&newwindow=1&safe=strict&es_sm=93&biw=1025&bih=225&ei=pnAvVZK8CM6xaarEgJgG&start={1}&sa=N&hl=zh-cn";
+
+        public Google()
+        {
+            base.PageSize = 10;
+        }
+
+        List<GridRowItem> ParseResult(HtmlNode hn)
+        {
+            if (hn != null)
+            {
+                var lis = hn.SelectNodes("//li[@class='g']/h3/a");
+
+                if (lis != null && lis.Count > 0)
+                {
+                    var ls = new List<GridRowItem>();
+                    foreach (var h2 in lis)
+                    {
+                        string title = HttpUtility.HtmlDecode(h2.InnerText);
+                        string href = h2.Attributes["href"].Value;
+                        int start = href.IndexOf("/url?q=");
+                        string url = "";
+                        if (start > -1)
+                        {
+                            url = base.ParseUrl(HttpUtility.HtmlDecode(href.Substring(start + 7)));
+                        }
+                        ls.Add(new GridRowItem
+                        {
+                            Title = title,
+                            Url = url
+                        });
+                    }
+
+                    if (!base.Cached.ContainsKey(base.CurrentPage))
+                    {
+                        base.Cached.Add(base.CurrentPage, ls);
+                    }
+
+                    return ls;
+                }
+            }
+
+            return null;
+        }
+
+        public override async Task<List<GridRowItem>> Search(string keyword)
+        {
+            List<GridRowItem> ls = null;
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                string url = string.Format(SearchUrlTemplate, keyword, 1);
+                using (var client = new HttpClient())
+                {
+                    var res = await client.GetAsync(url);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string html = await res.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            var hd = new HtmlDocument();
+                            hd.LoadHtml(html);
+                            var ires = hd.GetElementbyId("ires");
+                            if (ires != null)
+                            {
+                                var resultStats = hd.GetElementbyId("resultStats");
+                                if (resultStats != null)
+                                {
+                                    int start = resultStats.InnerText.IndexOf("结果");
+                                    if (start > -1)
+                                    {
+                                        base.ResultStatus = resultStats.InnerText.Substring(0, start) + "结果";
+                                    }
+                                    else
+                                    {
+                                        base.ResultStatus = resultStats.InnerText;
+                                    }
+
+                                    base.TotalPage = ParseTotal(base.ResultStatus) / PageSize;
+                                }
+
+                                ls = ParseResult(hd.DocumentNode);
+                            }
+                        }
+
+                        base.Keyword = keyword;
+                    }
+                }
+            }
+
+            return ls;
+        }
+
+        public override async Task<List<GridRowItem>> PageUp()
+        {
+            List<GridRowItem> ls = null;
+
+            if (base.Cached.ContainsKey(CurrentPage - 1))
+            {
+                CurrentPage--;
+                ls = base.Cached[CurrentPage];
+            }
+            else if (CurrentPage <= TotalPage)
+            {
+                int start = (CurrentPage - 2) * PageSize;
+                string url = string.Format(SearchUrlTemplate, base.Keyword, start);
+
+                using (var client = new HttpClient())
+                {
+                    var res = await client.GetAsync(url);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string html = await res.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            var hd = new HtmlDocument();
+                            hd.LoadHtml(html);
+                            var ires = hd.GetElementbyId("ires");
+                            if (ires != null)
+                            {
+                                CurrentPage--;
+                                ls = ParseResult(hd.DocumentNode);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ls;
+        }
+
+        public override async Task<List<GridRowItem>> PageDown()
+        {
+            List<GridRowItem> ls = null;
+
+            if (base.Cached.ContainsKey(CurrentPage + 1))
+            {
+                CurrentPage++;
+                ls = base.Cached[CurrentPage];
+            }
+            else if (CurrentPage >= 1)
+            {
+                int start = (CurrentPage - 1) * PageSize;
+                string url = string.Format(SearchUrlTemplate, base.Keyword, start);
+
+                using (var client = new HttpClient())
+                {
+                    var res = await client.GetAsync(url);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        string html = await res.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            var hd = new HtmlDocument();
+                            hd.LoadHtml(html);
+                            var ires = hd.GetElementbyId("ires");
+                            if (ires != null)
+                            {
+                                CurrentPage++;
+                                ls = ParseResult(hd.DocumentNode);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ls;
+        }
+    }
+}
